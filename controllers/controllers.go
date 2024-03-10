558b2ac3baa51fccceed4005b1777461bb42d625
Praveenk8051 <praveenk8051@gmail.com>
2024-03-10 10:17:37 +0100

    updated controller

---
+++ controllers/controllers.go
@@ -0,0 +1,31 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+
+}
+
+func SignUp() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandleFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandleFunc {
+
+}
+
+func SearchProduct() gin.HandleFunc {
+
+}
+
+func SearchProductByQuery() gin.HandleFunc {
+
+}
