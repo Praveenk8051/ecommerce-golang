232c2163d2f7e2e64fac1396536897bd6763497e
Krishna Murthy Praveen (AE/MFD1) <Praveen.KrishnaMurthy2@de.bosch.com>
2024-03-15 15:19:35 +0100

    complete db

--- database/databasetup.go
+++ database/databasetup.go
@@ -37,11 +37,16 @@ func DBSet() *mongo.Client{
 
 
 	var Client *mongo.Client = DBSet()
-func UserData(client *mongo.Client, collectionname string) *mongo.Collection{
 
+
+func UserData(client *mongo.Client, collectionname string) *mongo.Collection{
+	var collection *mongo.Collection = client.Database("Ecommerce").collection(collectionName)
+	return collection
 }
 
 
 func ProductData(client *mongo.Client, collectionname string) *mongo.Collection{
+	var productCollection
+
 
 }
