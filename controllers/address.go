1621ae0a2126ae3ead7c37e48e59928a85f450d7
Krishna Murthy Praveen (AE/MFD1) <Praveen.KrishnaMurthy2@de.bosch.com>
2024-03-15 14:30:20 +0100

    add more code

---
+++ controllers/address.go
@@ -0,0 +1,22 @@
+package controllers
+
+
+import(
+
+)
+
+func AddAddress() gin.HandlerFunc{
+
+}
+
+func EditHomeAddress() gin.HandlerFunc{
+
+}
+
+func EditWorkAddress() gin.HandlerFunc{
+
+}
+
+func DeleteAddress() gin.HandlerFunc{
+
+}
