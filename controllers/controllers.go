fa8f1d357223b0fe87df5cf8c30e9ccca4e8b586
Krishna Murthy Praveen (AE/MFD1) <Praveen.KrishnaMurthy2@de.bosch.com>
2024-03-15 16:16:46 +0100

    added siginou

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -50,10 +50,32 @@ func SignUp() gin.HandlerFunc {
 		return
 	}
 
+	password := HashPassword(user.Password)
+	user.Password := &password
+
+	user.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	user.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	user.ID = primitive.NewObjectID()
+	user.User_ID = user.ID.Hex()
+
+	token, refresh_token, _ = generate.TokenGenerator(*user.Email, *user.First_Name, *user.Last_Name,*user.User_ID)
+	user.Token = &token
+	user.Refresh_Token = &refresh_token
+	user.UserCart = make([]models.ProductUser, 0)
+	user.Address_Details = make([]models.Address, 0)
+	user.Order_Status = make([]models.Order, 0)
+	_, inserterr := UserCollection.InsertOne(ctx, user)
+	if inserterr != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Did not create User"})
+		return
+	}
+	defer cancel()
+	c.JSON(http.StatusCreated, "Successfully signed in")
 }
 }
 
 func Login() gin.HandleFunc {
+	
 
 }
 
