629a1360e219b28d4d8898337f51d11d9895e94a
Praveenk8051 <praveenk8051@gmail.com>
2024-03-24 17:21:13 +0100

    added delete address

--- controllers/address.go
+++ controllers/address.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func AddAddress() gin.HandlerFunc {
 
@@ -15,5 +19,17 @@ func EditWorkAddress() gin.HandlerFunc {
 }
 
 func DeleteAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
 
+		if user_id == ""{
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error":"Invalid Search Index"})
+			c.Abort()
+			return 
+		}
+
+		addresses := 
+
+	}
 }
