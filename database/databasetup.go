69186679607ef4d37b80bc855dc60276a4a74359
Praveenk8051 <praveenk8051@gmail.com>
2024-05-13 19:00:51 +0200

    removed depricated

--- database/databasetup.go
+++ database/databasetup.go
@@ -11,13 +11,9 @@ import (
 )
 
 func DBSet() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://development:testpassword@localhost:27017"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
-	err = client.Connect(ctx)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://development:testpassword@localhost:27017"))
 	if err != nil {
 		log.Fatal(err)
 	}
